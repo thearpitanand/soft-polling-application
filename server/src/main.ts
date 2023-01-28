edba5bb92ea17fe1f4e70ebf2a41e11d82148174
Arpit Anand <arpitssanand@gmail.com>
2023-01-28 21:54:22 +0530

    feat: add socket-io gateway configuration

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,17 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { SocketIOAdapter } from './socket-io-adapter';
 
 async function bootstrap() {
   const logger = new Logger('Main (main.ts)');
-  const app = await NestFactory.create(AppModule, {
-    cors: {
-      origin: [
-        'http://localhost:8080',
-        /^http:\/\/192\.168\.1\.([1-9]|[1-9]\d):8080$/,
-      ],
-    },
-  });
+  const app = await NestFactory.create(AppModule);
 
   const configService = app.get(ConfigService);
   const clientPort = parseInt(configService.get('CLIENT_PORT'));
@@ -24,6 +18,8 @@ async function bootstrap() {
       new RegExp(`/^http:\/\/192\.168\.1\.([1-9]|[1-9]\d):${clientPort}$/`),
     ],
   });
+
+  app.useWebSocketAdapter(new SocketIOAdapter(app, configService));
 
   await app.listen(port);
 
--- server/src/polls/polls.module.ts
+++ server/src/polls/polls.module.ts
@@ -3,12 +3,13 @@ import { ConfigModule } from '@nestjs/config';
 import { jwtModule } from 'src/jwt/modules.config';
 import { redisModule } from 'src/redis/modules.config';
 import { PollsController } from './polls.controller';
+import { PollsGateway } from './polls.gateway';
 import { PollsRepository } from './polls.repository';
 import { PollsService } from './polls.service';
 
 @Module({
   imports: [ConfigModule, redisModule, jwtModule],
   controllers: [PollsController],
-  providers: [PollsService, PollsRepository],
+  providers: [PollsService, PollsRepository, PollsGateway],
 })
 export class PollsModule {}
