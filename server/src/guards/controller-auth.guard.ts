21843703492f5490d1ea3311e860668f77bf8dc7
Arpit Anand <arpitssanand@gmail.com>
2023-01-29 11:49:09 +0530

    feat: add authentication middleware for web sockets

--- server/src/guards/controller-auth.guard.ts
+++ server/src/guards/controller-auth.guard.ts
@@ -6,13 +6,14 @@ import {
   Logger,
 } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import { RequestWithAuth } from 'src/polls/types';
 
 @Injectable()
 export class ControllerAuthGuard implements CanActivate {
   private readonly logger = new Logger(ControllerAuthGuard.name);
   constructor(private readonly jwtService: JwtService) {}
   canActivate(context: ExecutionContext): boolean | Promise<boolean> {
-    const request = context.switchToHttp().getRequest();
+    const request: RequestWithAuth = context.switchToHttp().getRequest();
 
     this.logger.debug(
       `Checking for auth token on request body: ${request.body}`,
--- server/src/polls/polls.gateway.ts
+++ server/src/polls/polls.gateway.ts
@@ -1,16 +1,50 @@
 import { Logger } from '@nestjs/common';
-import { OnGatewayInit, WebSocketGateway } from '@nestjs/websockets';
+import {
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Namespace } from 'socket.io';
 import { PollsService } from './polls.service';
+import { SocketWithAuth } from './types';
 
 @WebSocketGateway({
   namespace: 'polls',
 })
-export class PollsGateway implements OnGatewayInit {
+export class PollsGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
   private readonly logger = new Logger(PollsGateway.name);
   constructor(private readonly pollsService: PollsService) {}
+
+  @WebSocketServer() io: Namespace;
 
   // Gateway initialized (provide in module and instantiated)
   afterInit(): void {
     this.logger.log(`Websoket Gateway initialized`);
+  }
+
+  handleConnection(client: SocketWithAuth) {
+    const socket = this.io.sockets;
+
+    this.logger.debug(
+      `Socket connected with userID: ${client.userID}, pollID: ${client.pollID} and name: "${client.name}"`,
+    );
+
+    this.logger.log(`Web Socket Client with id: ${client.id} connected!`);
+    this.logger.debug(`Number of connected web sockets: ${socket.size}`);
+
+    this.io.emit('hello', `Hello from ${client.id}`);
+  }
+
+  handleDisconnect(client: SocketWithAuth) {
+    const socket = this.io.sockets;
+
+    this.logger.log(`Web Socket Client with id: ${client.id} disconnected!`);
+    this.logger.debug(`Number of connected web sockets: ${socket.size}`);
+
+    this.io.emit('goodbye', `goodbye from ${client.id}`);
   }
 }
